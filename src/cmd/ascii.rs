4972d5469e9b25b1d31dc1209444ee4918c6cda0
ronakgh97 <ronakgh97@gmail.com>
2025-12-30 16:13:23 +0530

    Add ASCII art display; include total configs and sessions count, added chrono crate

--- src/cmd/ascii.rs
+++ src/cmd/ascii.rs
@@ -1,3 +1,5 @@
+use crate::core::config::get_default_config_path;
+use anyhow::Result;
 use colored::Colorize;
 
 pub async fn run_ascii_art() {
@@ -10,9 +12,27 @@ pub async fn run_ascii_art() {
                    ▀▀▀                           
     "#;
 
-    println!("{}\n\n", ascii_art.to_string().magenta());
+    println!("{}\n", ascii_art.to_string().magenta());
+    let total_configs = get_total_configs().unwrap_or_else(|_| 0);
+    println!(" Total Configs: {}\n", total_configs.to_string().cyan());
+    let total_sessions = get_total_sessions().unwrap_or_else(|_| 0);
+    println!(" Total Sessions: {}\n", total_sessions.to_string().cyan());
     println!(
         " Github: {}\n",
         "https://github.com/ronakg/r-agent".to_string().cyan()
     );
+}
+
+fn get_total_configs() -> Result<usize> {
+    let configs_dir = get_default_config_path()?;
+    let entries = std::fs::read_dir(configs_dir)?;
+    let count = entries.count();
+    Ok(count)
+}
+
+fn get_total_sessions() -> Result<usize> {
+    let sessions_dir = crate::core::session::get_default_session_path()?;
+    let entries = std::fs::read_dir(sessions_dir)?;
+    let count = entries.count();
+    Ok(count)
 }
